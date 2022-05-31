2a1b4900b7cf959123aeca0e64531b600da34e32
Tami Takada <ttakada20@stuy.edu>
2022-05-31 08:24:26 -0400

    pixel upsert & find

--- myspace/database.py
+++ myspace/database.py
@@ -13,20 +13,20 @@ def db_setup():
     c = db.cursor()
 
     command = """CREATE TABLE IF NOT EXISTS users (
-        id         			INTEGER PRIMARY KEY,
+        id         		INTEGER PRIMARY KEY,
         username     		TEXT,
-        password 			VARBINARY,
-        salt				BINARY,
+        password 		VARBINARY,
+        salt			BINARY,
         last_contribution 	DATETIME
         );"""
     c.execute(command)
     
     command = """CREATE TABLE IF NOT EXISTS canvas (
-        id		INTEGER PRIMARY KEY,
-        x     	TEXT,
-        y 		TEXT,
+        id	INTEGER PRIMARY KEY,
+        x     	INTEGER KEY,
+        y 	INTEGER KEY,
         color	TEXT
-        );"""
+    );"""
     c.execute(command)
 
     db.commit()
@@ -58,9 +58,9 @@ def add_user(username: str, password: str):
     
     key = hashlib.pbkdf2_hmac(
         'sha256',
-		password.encode('utf-8'),
-		salt,
-		100000
+        password.encode('utf-8'),
+        salt,
+        100000
     )
 
     command = "INSERT INTO users (id, username, password, salt) VALUES (?, ?, ?, ?)"
@@ -71,24 +71,24 @@ def add_user(username: str, password: str):
 
 
 """
-	Returns:
-		0 - No user with that username found
-		1 - User found but wrong password
-		2 - Successful login
+    Returns:
+	0 - No user with that username found
+	1 - User found but wrong password
+	2 - Successful login
 """
 def attempt_login(username: str, password: str):
-	user = find_user_by_username(username)
-	if user:
-		salt = user[3]
-		key = hashlib.pbkdf2_hmac(
-		    'sha256',
-			password.encode('utf-8'),
-			salt,
-			100000
-		)
-		if key == user[2]: return 2
-		else: return 1
-	return 0
+    user = find_user_by_username(username)
+    if user:
+        salt = user[3]
+        key = hashlib.pbkdf2_hmac(
+            'sha256',
+            password.encode('utf-8'),
+            salt,
+            100000
+        )
+        if key == user[2]: return 2
+        else: return 1
+    return 0
 
 
 def find_user_by_username(username: str):
@@ -101,3 +101,30 @@ def find_user_by_username(username: str):
     db.close()
 
     return user
+    
+def upsert_pixel(x: int, y: int, color: str):
+    db = get_db()
+    c = db.cursor()
+    
+    pixel = find_pixel_from_coordinates(x, y)
+    if pixel:
+    	command = "UPDATE canvas SET x = ?, y = ?, color = ? WHERE id = ?"
+    	c.execute(command, (x, y, color, pixel[0]))
+    else: 
+    	pixel_id = generate_unique_id("canvas")
+    	command = "INSERT INTO canvas (id, x, y, color) VALUES (?, ?, ?, ?)"
+    	c.execute(command, (pixel_id, x, y, color,))
+
+    db.commit()
+    db.close()
+    
+def find_pixel_from_coordinates(x: int, y: int):
+    db = get_db()
+    c = db.cursor()
+
+    command = "SELECT * FROM canvas WHERE x = ? AND y = ?"
+    pixel = c.execute(command, (x, y)).fetchone()
+
+    db.close()
+
+    return pixel
