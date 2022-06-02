40eceeb32f71c5cd957a329dde2071246894da70
qiqimonkey4300 <yueqi04@gmail.com>
2022-06-02 18:41:18 -0400

    pixels snap to grid + 10s cooldown :D

--- myspace/static/js/canvas.js
+++ myspace/static/js/canvas.js
@@ -1,4 +1,5 @@
 var c = document.getElementById("slate");
+var currentTime = new Date();
 
 //instantiate a CanvasRenderingContext2D object
 var ctx = c.getContext("2d");
@@ -7,12 +8,16 @@ var color = "red";
 var mode = true;
 
 var drawRect = function(e) {
+  checkTime()
   if (mode) {
     var mouseX = e.offsetX;
     var mouseY = e.offsetY;
     console.log("mouseclick registered at ", mouseX, mouseY);
 
     var fillingRect = new Path2D(); //instead of beginPath
+    // stays within grid
+    mouseX -= mouseX % 10;
+    mouseY -= mouseY % 10;
     fillingRect.moveTo(mouseX, mouseY);
     fillingRect.lineTo(mouseX+10, mouseY);
     fillingRect.lineTo(mouseX+10, mouseY+10);
@@ -23,6 +28,16 @@ var drawRect = function(e) {
     ctx.fillStyle = color;
     ctx.fill(fillingRect);
     mode = false;
+    currentTime = new Date();
+  }
+}
+
+function checkTime() {
+  var checkNow = new Date();
+  var timeDiff = (checkNow - currentTime) / 1000;
+  console.log("timeDiff: ", timeDiff);
+  if (timeDiff >= 10) {
+    mode = true;
   }
 }
 
