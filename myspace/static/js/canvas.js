98b7ca6e2249d7249c02c4814f16c408b75462e6
dib1000 <dib5017@gmail.com>
2022-06-02 11:10:51 -0400

    toggle

--- myspace/static/js/canvas.js
+++ myspace/static/js/canvas.js
@@ -4,23 +4,26 @@ var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 
 var color = "red";
-var mode = "true"
+var mode = true;
 
 var drawRect = function(e) {
-  var mouseX = e.offsetX;
-  var mouseY = e.offsetY;
-  console.log("mouseclick registered at ", mouseX, mouseY);
+  if (mode) {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+    console.log("mouseclick registered at ", mouseX, mouseY);
 
-  var fillingRect = new Path2D(); //instead of beginPath
-  fillingRect.moveTo(mouseX, mouseY);
-  fillingRect.lineTo(mouseX+10, mouseY);
-  fillingRect.lineTo(mouseX+10, mouseY+10);
-  fillingRect.lineTo(mouseX, mouseY+10);
-  fillingRect.lineTo(mouseX, mouseY);
-  fillingRect.closePath();
+    var fillingRect = new Path2D(); //instead of beginPath
+    fillingRect.moveTo(mouseX, mouseY);
+    fillingRect.lineTo(mouseX+10, mouseY);
+    fillingRect.lineTo(mouseX+10, mouseY+10);
+    fillingRect.lineTo(mouseX, mouseY+10);
+    fillingRect.lineTo(mouseX, mouseY);
+    fillingRect.closePath();
 
-  ctx.fillStyle = color;
-  ctx.fill(fillingRect);
+    ctx.fillStyle = color;
+    ctx.fill(fillingRect);
+    mode = false;
+  }
 }
 
 function changeColor(c) {
