cfac1e8196966dc1acf7fe849405ec3beaf4a099
dib1000 <dib5017@gmail.com>
2022-06-02 08:37:35 -0400

    draw and color

---
+++ myspace/static/js/canvas.js
@@ -0,0 +1,31 @@
+var c = document.getElementById("slate");
+
+//instantiate a CanvasRenderingContext2D object
+var ctx = c.getContext("2d");
+
+var color = "red";
+var mode = "true"
+
+var drawRect = function(e) {
+  var mouseX = e.offsetX;
+  var mouseY = e.offsetY;
+  console.log("mouseclick registered at ", mouseX, mouseY);
+
+  var fillingRect = new Path2D(); //instead of beginPath
+  fillingRect.moveTo(mouseX, mouseY);
+  fillingRect.lineTo(mouseX+10, mouseY);
+  fillingRect.lineTo(mouseX+10, mouseY+10);
+  fillingRect.lineTo(mouseX, mouseY+10);
+  fillingRect.lineTo(mouseX, mouseY);
+  fillingRect.closePath();
+
+  ctx.fillStyle = color;
+  ctx.fill(fillingRect);
+}
+
+function changeColor(c) {
+  color = c;
+  console.log(color);
+}
+
+c.addEventListener("click", drawRect);
